e646e7c63f3526e7a5bb3e4bb582ba800e6df3a1
DVitsme <derrick.valentine@gmail.com>
2023-05-27 13:12:40 -0700

    setting up auth in navbar

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,11 +1,30 @@
 'use client';
 import { Session } from 'next-auth';
 import { signIn } from 'next-auth/react';
+import Image from 'next/image';
 
 const Nav = ({ user }: Session) => {
   return (
     <nav>
       <h1>Sanity Check</h1>
+      <ul>
+        <li>Products</li>
+        {user ? (
+          <li>
+            <Image
+              alt={user.name as string}
+              src={user?.image as string}
+              height={48}
+              width={48}
+              className="rounded-full"
+            />
+          </li>
+        ) : (
+          <li>
+            <button onClick={() => signIn()}>Sign In</button>
+          </li>
+        )}
+      </ul>
     </nav>
   );
 };
